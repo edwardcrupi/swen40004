4ab1153c8d85c2badd0b0f219b937c87833296d0
Edward Crupi <e.crupi@student.unimelb.edu.au>
2015-03-27 00:34:25 +1100

    Just added comments

--- CellularAutomaton.java
+++ CellularAutomaton.java
@@ -2,12 +2,15 @@ public class CellularAutomaton{
 	static Grid grid;
 	
 	public static void main(String[] args){
+		//Initial Seed from Command Line
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
 		boolean fill;
 		fill = args[2].equals("true") ? true : false;
 		int ticks = Integer.parseInt(args[3]);
 		grid = new Grid(x,y,fill);
+		
+		//Update Loop
 		for(int i = 0; i < ticks; i++){
 			System.out.println("t = " + (i+1));
 			grid.update();
